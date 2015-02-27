b898a14fac3aaf68c51fd3c067fd02a6c462acfa
Stephan Jaensch <sjaensch@yelp.com>
2015-02-27 04:52:58 -0800

    Fix merge conflict resolution error

--- pushmanager/tests/test_template_pushes.py
+++ pushmanager/tests/test_template_pushes.py
@@ -8,7 +8,6 @@ class PushesTemplateTest(TemplateTestCase):
     pushes_page = 'pushes.html'
     new_push_page = 'new-push.html'
 
-<<<<<<< HEAD
     def render_pushes_page(self, page_title='Pushes', pushes=[], pushes_per_page=50, offset=0):
         return self.render_etree(
             self.pushes_page,
